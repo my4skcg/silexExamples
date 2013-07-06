6de18888a3d4c56f7c945c08e06db36d29a38055
Marion <my4skcg@gmail.com>
2013-07-05 19:26:19 -0500

    changed directory for allowOverride

--- app/app.php
+++ app/app.php
@@ -3,25 +3,24 @@
 // Bootstrap
 require __DIR__ . DIRECTORY_SEPARATOR . 'bootstrap.php';
 
-// Catch faulty requests (think 404) - @see http://silex.sensiolabs.org/doc/usage.html#error-handlers
+// Catch faulty requests (think 404)
 $app->error(function (\Exception $e, $code) {
 	if ($code == 404) {
-		return 'Please visit <code>/hello/<em>name</em></code> to get your hello world mojo on!';
+		return 'Please visit <code>/olleh/<em>name</em></code> to get your hello world mojo on!';
 	} else {
 		return 'Shenanigans! Something went horribly wrong';
 	}
 });
 
-// Basic Routing
+// Routing
 $app->get('/', function(Silex\Application $app) {
-	return $app->redirect($app['request']->getBaseUrl() . '/hello');
+	return $app->redirect($app['request']->getBaseUrl() . '/olleh');
 });
 
-// Dynamic Routing: Catch requests to /hello/{name} - @see http://silex.sensiolabs.org/doc/usage.html#dynamic-routing
-$app->get('/hello/{name}', function(Silex\Application $app, $name) {
-	return 'Hello ' . $app->escape($name) . '!';
-});
-
-// Not demonstrated: other request methods
-// ->post() - matches POST - @see http://silex.sensiolabs.org/doc/usage.html#example-post-route
-// ->match() - matches *all* types of requests. May be combined with ->method() - @see http://silex.sensiolabs.org/doc/usage.html#other-methods
+// Dynamic Routing: Catch requests to /olleh and /olleh/{name}
+$app->get('/olleh/{name}', function(Silex\Application $app, $name) {
+	return '!' . $app->escape($name) . ' olleH';
+})
+->assert('name', '\w+') // Routing Requirements (only allow alphanumeric characters) - @see http://silex.sensiolabs.org/doc/usage.html#requirements
+->convert('name', function($name) { return strrev($name); }) // Route variable converters (convert value before usage) - @see http://silex.sensiolabs.org/doc/usage.html#route-variables-converters
+->value('name', 'stranger'); // Default Values - @see http://silex.sensiolabs.org/doc/usage.html#default-values
